5da13165268338a59840b49e6dda131530bf6557
saiki <127001sudais01@gmail.com>
2025-03-13 16:30:45 +0800

    missed on previous comimt

--- src/utils/streamAddressesFromFile.ts
+++ src/utils/streamAddressesFromFile.ts
@@ -1,52 +1,51 @@
-import { isAddress } from "@solana/kit";
-import pLimit from "p-limit";
 import * as fs from "fs";
 import * as readline from "readline";
 import * as path from "path";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import { insertAddressIntoDatabase } from "./insertAddressIntoDatabase.js";
+import { validateAddress } from "./helpers.js";
 
 export async function streamAddressesFromFile(
-  fileName: string,
-  concurrency: number
+  fileName: string
 ): Promise<boolean> {
   const filePath = path.join(process.cwd(), fileName);
   const fileStream = fs.createReadStream(filePath);
-
+  const DB_NAME = path.join(process.cwd(), "SOLANA_ADDRESSES.DB");
+  const batchLimit = 2;
+  const db = await open({
+    filename: DB_NAME,
+    driver: sqlite3.Database,
+  });
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
-
-  const limit = pLimit(concurrency);
-
-  const validationPromises: Promise<void>[] = [];
+  let batchedAddresses: string[] = [];
   let invalidAddressCount = 0;
 
   for await (const line of rl) {
     const address = line.trim();
-
     if (!address) continue;
-
-    validationPromises.push(
-      limit(async () => {
-        const isValid = validateAddress(address);
-        if (!isValid) {
-          invalidAddressCount++;
-        }
-      })
-    );
+    if (validateAddress(address)) {
+      batchedAddresses.push(address);
+      if (batchedAddresses.length >= batchLimit) {
+        await insertAddressIntoDatabase(batchedAddresses, db).then(() => {
+          console.log("A batch of address added.");
+          batchedAddresses = [];
+        });
+      }
+    } else {
+      invalidAddressCount++;
+    }
   }
 
-  rl.close();
-
-  await Promise.all(validationPromises);
-
-  console.log(
-    `All addresses processed. Total invalid Addresses: ${invalidAddressCount}`
-  );
-
+  if (batchedAddresses.length > 0) {
+    await insertAddressIntoDatabase(batchedAddresses, db).then(() => {
+      console.log("Adding final batch");
+    });
+  }
+  console.log(batchedAddresses.length);
+  console.log("total invalid address count: ", invalidAddressCount);
   return true;
-}
-
-function validateAddress(address: string): boolean {
-  return isAddress(address);
 }
